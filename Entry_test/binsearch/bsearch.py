a8115e38168701eded9d13a1b1c190e0e439304d
npavel25 <52123646+npavel25@users.noreply.github.com>
2019-06-23 14:30:38 +0300

    added exception
    throw exception if given value is not in the list

--- Entry_test/binsearch/bsearch.py
+++ Entry_test/binsearch/bsearch.py
@@ -1,6 +1,12 @@
+class EmptyListException(Exception):
+    pass
+
+#recursive version. Not the best one
 def bsearch(lst, value):
     print("list: ", lst);
     l = len(lst);
+    if 0 == l:
+        raise EmptyListException(str(value) + " is not in the list.");
     mid = int(l / 2)
     print("length: ", l, "middle: ", mid);
     if value < lst[mid]:
@@ -12,6 +18,17 @@ def bsearch(lst, value):
         res = mid;
     return res;
     
-lst = [1,2,3,4,5,6];
-res = bsearch(lst,3);
-print("bsearch: ", res);
+try:
+    lst = [1,2,3,4,5,6];
+    res = bsearch(lst,3);
+    print("bsearch: ", res);
+except Exception as e:
+    print(e);
+try:    
+    lst = [2,4,6,8,10,12];
+    res = bsearch(lst,3);
+    print("bsearch: ", res);
+except Exception as e:
+    print(e);
+        
+
